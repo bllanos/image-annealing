1b38d37063f8bc0b4f3267cb76237b4ee35e0031
Bernard Llanos <bernard@mojow.ai>
2020-08-30 09:53:27 -0600

    Printing command-line arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut args = env::args().peekable();
+    args.next()
+        .expect("No command-line arguments (not even the program name)");
+    let filename = match args.next() {
+        Some(val) => val,
+        None => Err("Expected at least one command-line argument for an image filename")?,
+    };
+    println!("Filename: {}", filename);
+    if let Some(_) = args.peek() {
+        println!("Additional arguments:");
+        for argument in args {
+            println!("\t{}", argument);
+        }
+    }
+
+    Ok(())
 }
