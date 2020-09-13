0700133392127303c5b98e12b09a196472c992b5
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2020-09-13 10:45:59 -0600

    Parsing an image rectangle.
    - Refactored command-line argument parsing into a function

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,20 @@
 use std::env;
 use std::error::Error;
+use std::fmt;
 
 use std::path::Path;
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut args = env::args().peekable();
-    args.next()
-        .expect("No command-line arguments (not even the program name)");
-    let filename = args
-        .next()
-        .ok_or("Expected at least one command-line argument for an image filename.")?;
-    println!("Filename: {}", filename);
-    if let Some(_) = args.peek() {
-        println!("Additional arguments:");
-        for argument in args {
-            println!("\t{}", argument);
-        }
-    }
+    let (filename, rect) = parse_args()?;
+
+    rect.iter()
+        .for_each(|x| println!("Image rectangle is {:?}", x));
 
     let filepath = Path::new(&filename);
     if !filepath.is_file() {
         Err(format!(
-            "Image file '{}' does not exist in the filesystem.",
+            "Image file {} does not exist in the filesystem.",
             filename
         ))?
     }
@@ -55,4 +47,76 @@ fn main() -> Result<(), Box<dyn Error>> {
     img.save(output_path)?;
 
     Ok(())
+}
+
+#[derive(Debug, Clone)]
+struct InvalidCornersError;
+
+impl fmt::Display for InvalidCornersError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "invalid specification of top left and bottom right image rectangle corners"
+        )
+    }
+}
+
+impl Error for InvalidCornersError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    x: u32,
+    y: u32,
+    width: u32,
+    height: u32,
+}
+
+const N_CORNERS: usize = 4;
+
+impl Rectangle {
+    fn from_corners(x1: u32, y1: u32, x2: u32, y2: u32) -> Result<Rectangle, InvalidCornersError> {
+        if x1 <= x2 && y1 <= y2 {
+            Ok(Rectangle {
+                x: x1,
+                y: y1,
+                width: x2 - x1,
+                height: y2 - y1,
+            })
+        } else {
+            Err(InvalidCornersError)
+        }
+    }
+}
+
+fn parse_args() -> Result<(String, Option<Rectangle>), Box<dyn Error>> {
+    let mut args = env::args();
+    args.next()
+        .expect("No command-line arguments (not even the program name)");
+    let filename = args
+        .next()
+        .ok_or("Expected at least one command-line argument for an image filename.")?;
+    if let Some(arg) = args.next() {
+        if arg == "--rect" {
+            let v: Vec<_> = args
+                .take(N_CORNERS)
+                .map(|x| x.parse::<u32>())
+                .filter(|x| x.is_ok())
+                .map(Result::unwrap)
+                .collect();
+            if v.len() == N_CORNERS {
+                let rect = Rectangle::from_corners(v[0], v[1], v[2], v[3])?;
+                Ok((filename, Some(rect)))
+            } else {
+                Err("Failed to parse corners for image rectangle")?
+            }
+        } else {
+            Ok((filename, None))
+        }
+    } else {
+        Ok((filename, None))
+    }
 }
