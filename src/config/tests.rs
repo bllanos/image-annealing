1c13355b9ca259220a6307ce190cc76e97b8141c
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2020-09-20 20:19:19 -0600

    Moved unit tests for the config module to a separate file.

---
+++ src/config/tests.rs
@@ -0,0 +1,97 @@
+mod parse_args {
+    use crate::config::parse_args;
+    use crate::image_utils::rectangle::Rectangle;
+    use std::error::Error;
+
+    #[test]
+    #[should_panic(expected = "No arguments (not even the program name)")]
+    fn empty_input() {
+        let v: Vec<String> = Vec::new();
+        println!("{:?}", parse_args(v));
+    }
+
+    #[test]
+    fn no_image() {
+        let v = vec!["one"];
+        let v_strings: Vec<String> = v.into_iter().map(String::from).collect();
+        let r = parse_args(v_strings);
+        r.expect_err("At least one argument should be required");
+    }
+
+    #[test]
+    fn single_arg() -> Result<(), Box<dyn Error>> {
+        let v = vec!["one", "two"];
+        let v_strings: Vec<String> = v.into_iter().map(String::from).collect();
+        let r = parse_args(v_strings.clone())?;
+        assert_eq!(r.0, v_strings[1]);
+        // Option::expect_none is an experimental feature
+        assert!(r.1.is_none());
+        Ok(())
+    }
+
+    #[test]
+    fn irrelevant_args() -> Result<(), Box<dyn Error>> {
+        let v = vec!["one", "two", "three", "1", "2", "3", "4"];
+        let v_strings: Vec<String> = v.into_iter().map(String::from).collect();
+        let r = parse_args(v_strings.clone())?;
+        assert_eq!(r.0, v_strings[1]);
+        assert!(
+            r.1.is_none(),
+            "A Rectangle should not be returned if `--rect` is not the second argument."
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn incomplete_rect() {
+        let v = vec!["one", "two", "--rect"];
+        let v_strings: Vec<String> = v.into_iter().map(String::from).collect();
+        let r = parse_args(v_strings);
+        r.expect_err("`--rect` with no following coordinates should result in an error");
+    }
+
+    #[test]
+    fn bad_rect_parse() {
+        let v = vec!["one", "two", "--rect", "a", "b", "1", "2"];
+        let v_strings: Vec<String> = v.into_iter().map(String::from).collect();
+        let r = parse_args(v_strings);
+        r.expect_err("`--rect` with non-numbers afterwards should be an error");
+    }
+
+    #[test]
+    fn bad_rect_negative() {
+        let v = vec!["one", "two", "--rect", "-1", "-1", "1", "2"];
+        let v_strings: Vec<String> = v.into_iter().map(String::from).collect();
+        let r = parse_args(v_strings);
+        r.expect_err("`--rect` with negative numbers afterwards should be an error");
+    }
+
+    #[test]
+    fn bad_rect_malformed() {
+        let v = vec!["one", "two", "--rect", "1", "2", "1", "0"];
+        let v_strings: Vec<String> = v.into_iter().map(String::from).collect();
+        let r = parse_args(v_strings);
+        r.expect_err("`--rect` impossible coordinates afterwards should be an error");
+    }
+
+    #[test]
+    fn bad_rect_empty() {
+        let v = vec!["one", "two", "--rect", "1", "0", "2", "0"];
+        let v_strings: Vec<String> = v.into_iter().map(String::from).collect();
+        let r = parse_args(v_strings);
+        r.expect_err("`--rect` empty rectangle coordinates afterwards should be an error");
+    }
+
+    #[test]
+    fn good_rect() -> Result<(), Box<dyn Error>> {
+        let v = vec!["one", "two", "--rect", "1", "0", "2", "5"];
+        let v_strings: Vec<String> = v.into_iter().map(String::from).collect();
+        let r = parse_args(v_strings.clone())?;
+        assert_eq!(r.0, v_strings[1]);
+        assert_eq!(
+            r.1.ok_or("Expected a valid rectangle")?,
+            Rectangle::from_corners(1, 0, 2, 5)?
+        );
+        Ok(())
+    }
+}
