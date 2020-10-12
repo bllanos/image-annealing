b7ebca0bdcf8dab17d4dabbce4d0bac24653db91
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2020-10-12 15:02:44 -0600

    Move tests that exercise multiple modules to integration tests

--- src/config/tests.rs
+++ src/config/tests.rs
@@ -1,6 +1,5 @@
 mod parse_args {
     use crate::config::parse_args;
-    use crate::image_utils::rectangle::Rectangle;
     use std::error::Error;
 
     #[test]
@@ -56,43 +55,6 @@ mod parse_args {
         let v_strings: Vec<String> = v.into_iter().map(String::from).collect();
         let r = parse_args(v_strings);
         r.expect_err("`--rect` with non-numbers afterwards should be an error");
-    }
-
-    #[test]
-    fn bad_rect_negative() {
-        let v = vec!["one", "two", "--rect", "-1", "-1", "1", "2"];
-        let v_strings: Vec<String> = v.into_iter().map(String::from).collect();
-        let r = parse_args(v_strings);
-        r.expect_err("`--rect` with negative numbers afterwards should be an error");
-    }
-
-    #[test]
-    fn bad_rect_malformed() {
-        let v = vec!["one", "two", "--rect", "1", "2", "1", "0"];
-        let v_strings: Vec<String> = v.into_iter().map(String::from).collect();
-        let r = parse_args(v_strings);
-        r.expect_err("`--rect` impossible coordinates afterwards should be an error");
-    }
-
-    #[test]
-    fn bad_rect_empty() {
-        let v = vec!["one", "two", "--rect", "1", "0", "2", "0"];
-        let v_strings: Vec<String> = v.into_iter().map(String::from).collect();
-        let r = parse_args(v_strings);
-        r.expect_err("`--rect` empty rectangle coordinates afterwards should be an error");
-    }
-
-    #[test]
-    fn good_rect() -> Result<(), Box<dyn Error>> {
-        let v = vec!["one", "two", "--rect", "1", "0", "2", "5"];
-        let v_strings: Vec<String> = v.into_iter().map(String::from).collect();
-        let r = parse_args(v_strings.clone())?;
-        assert_eq!(r.0, v_strings[1]);
-        assert_eq!(
-            r.1.ok_or("Expected a valid rectangle")?,
-            Rectangle::from_corners(1, 0, 2, 5)?
-        );
-        Ok(())
     }
 }
 
