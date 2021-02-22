c9393ec8cf194499d9f1de906de3ca646e411e44
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2021-02-21 21:04:58 -0700

    Additional utility functions for ImageDimensions
    - Convert 2D coordinates to a 1D index

--- image_annealing/src/image_utils/dimension/mod.rs
+++ image_annealing/src/image_utils/dimension/mod.rs
@@ -1,5 +1,8 @@
+use image::{ImageBuffer, Pixel};
+use std::convert::TryInto;
 use std::error::Error;
 use std::fmt;
+use std::ops::Deref;
 
 #[derive(Debug, Clone)]
 pub struct InvalidDimensionError;
@@ -11,6 +14,21 @@ impl fmt::Display for InvalidDimensionError {
 }
 
 impl Error for InvalidDimensionError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct OutOfBoundsCoordinatesError;
+
+impl fmt::Display for OutOfBoundsCoordinatesError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Coordinates are out of bounds")
+    }
+}
+
+impl Error for OutOfBoundsCoordinatesError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         None
     }
@@ -31,6 +49,18 @@ impl ImageDimensions {
         }
     }
 
+    pub fn from_image<P: Pixel, Container>(
+        image: &ImageBuffer<P, Container>,
+    ) -> Result<Self, InvalidDimensionError>
+    where
+        P: Pixel + 'static,
+        P::Subpixel: 'static,
+        Container: Deref<Target = [P::Subpixel]>,
+    {
+        let (width, height) = image.dimensions();
+        Self::new(width as usize, height as usize)
+    }
+
     pub fn width(&self) -> usize {
         self.width
     }
@@ -41,6 +71,30 @@ impl ImageDimensions {
 
     pub fn count(&self) -> usize {
         self.width.checked_mul(self.height).unwrap()
+    }
+
+    pub fn to_linear_index<T: TryInto<usize>>(
+        &self,
+        x: T,
+        y: T,
+    ) -> Result<usize, OutOfBoundsCoordinatesError> {
+        let x_usize = x.try_into().or(Err(OutOfBoundsCoordinatesError))?;
+        let y_usize = y.try_into().or(Err(OutOfBoundsCoordinatesError))?;
+        if x_usize >= self.width || y_usize >= self.height {
+            Err(OutOfBoundsCoordinatesError)
+        } else {
+            Ok(y_usize
+                .checked_mul(self.width)
+                .unwrap()
+                .checked_add(x_usize)
+                .unwrap())
+        }
+    }
+}
+
+impl fmt::Display for ImageDimensions {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "(width, height) = ({}, {})", self.width, self.height)
     }
 }
 
--- image_annealing/src/image_utils/dimension/tests.rs
+++ image_annealing/src/image_utils/dimension/tests.rs
@@ -1,4 +1,5 @@
 use super::ImageDimensions;
+use image::RgbaImage;
 use std::error::Error;
 
 #[test]
@@ -20,4 +21,66 @@ fn valid_dimensions() -> Result<(), Box<dyn Error>> {
     assert_eq!(dim.height(), 64);
     assert_eq!(dim.count(), 63 * 64);
     Ok(())
+}
+
+#[test]
+fn from_image() -> Result<(), Box<dyn Error>> {
+    let image = RgbaImage::new(4, 5);
+    let dim = ImageDimensions::from_image(&image)?;
+    assert_eq!(dim.width(), image.width() as usize);
+    assert_eq!(dim.height(), image.height() as usize);
+    Ok(())
+}
+
+mod to_linear_index {
+    use super::super::ImageDimensions;
+    use std::error::Error;
+
+    #[test]
+    fn negative_x() -> Result<(), Box<dyn Error>> {
+        let dim = ImageDimensions::new(3, 5)?;
+        let r = dim.to_linear_index(-1, 0);
+        r.expect_err("An error should be raised if the x-coordinate is negative");
+        Ok(())
+    }
+
+    #[test]
+    fn negative_y() -> Result<(), Box<dyn Error>> {
+        let dim = ImageDimensions::new(3, 5)?;
+        let r = dim.to_linear_index(0, -1);
+        r.expect_err("An error should be raised if the y-coordinate is negative");
+        Ok(())
+    }
+
+    #[test]
+    fn large_x() -> Result<(), Box<dyn Error>> {
+        let dim = ImageDimensions::new(3, 5)?;
+        let r = dim.to_linear_index(3, 0);
+        r.expect_err("An error should be raised if the x-coordinate is too large");
+        Ok(())
+    }
+
+    #[test]
+    fn large_y() -> Result<(), Box<dyn Error>> {
+        let dim = ImageDimensions::new(3, 5)?;
+        let r = dim.to_linear_index(0, 5);
+        r.expect_err("An error should be raised if the y-coordinate is too large");
+        Ok(())
+    }
+
+    #[test]
+    fn valid_coordinates() -> Result<(), Box<dyn Error>> {
+        let dim = ImageDimensions::new(3, 5)?;
+        let mut i = dim.to_linear_index(0, 0)?;
+        assert_eq!(i, 0);
+        i = dim.to_linear_index(1, 0)?;
+        assert_eq!(i, 1);
+        i = dim.to_linear_index(0, 1)?;
+        assert_eq!(i, 3);
+        i = dim.to_linear_index(1, 1)?;
+        assert_eq!(i, 4);
+        i = dim.to_linear_index(2, 4)?;
+        assert_eq!(i, 3 * 5 - 1);
+        Ok(())
+    }
 }
