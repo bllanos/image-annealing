c0136bbf7c9f5c4937a3e991ffb09f24e14f2fe7
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2021-04-11 10:09:16 -0600

    Replace shaderc compilation with Naga shader validation in the build

--- image_annealing/build.rs
+++ image_annealing/build.rs
@@ -1,84 +1,39 @@
-//! Reference: https://github.com/sotrh/learn-wgpu
+//! References:
+//! https://github.com/sotrh/learn-wgpu
+//! https://github.com/gfx-rs/naga/blob/master/bin/convert.rs
 
 use anyhow::*;
 use glob::glob;
 use rayon::prelude::*;
-use std::fs::{read_to_string, write};
-use std::path::{Path, PathBuf};
+use std::error::Error;
+use std::fs::read_to_string;
+use std::path::PathBuf;
+
+fn print_err(error: impl Error) {
+    eprintln!("\t{}:", error);
+    let mut e = error.source();
+    while let Some(source) = e {
+        eprintln!("\t\t{}", source);
+        e = source.source();
+    }
+}
 
 struct ShaderData {
     src: String,
     src_path: PathBuf,
-    spv_path: PathBuf,
-    kind: shaderc::ShaderKind,
 }
 
 impl ShaderData {
     pub fn load(src_path: PathBuf) -> Result<Self> {
-        let extension = src_path
-            .extension()
-            .context("File has no extension")?
-            .to_str()
-            .context("Extension cannot be converted to a string")?;
-        let kind = match extension {
-            "vert" => shaderc::ShaderKind::Vertex,
-            "frag" => shaderc::ShaderKind::Fragment,
-            "comp" => shaderc::ShaderKind::Compute,
-            _ => bail!("Unsupported shader extension: {}", src_path.display()),
-        };
-
         let src = read_to_string(&src_path)?;
-        let spv_path = src_path.with_extension(format!("{}.spv", extension));
-
-        Ok(Self {
-            src,
-            src_path,
-            spv_path,
-            kind,
-        })
-    }
-}
-
-// Reference: https://github.com/Wumpf/blub/blob/master/src/wgpu_utils/shader.rs
-fn include_directive_callback(
-    name: &str,
-    include_type: shaderc::IncludeType,
-    source_file: &str,
-    _depth: usize,
-) -> shaderc::IncludeCallbackResult {
-    match include_type {
-        shaderc::IncludeType::Relative => {
-            let path = Path::new(Path::new(source_file).parent().unwrap()).join(name);
-            match std::fs::read_to_string(&path) {
-                Ok(glsl_code) => {
-                    println!(
-                        "cargo:rerun-if-changed={}",
-                        path.as_os_str().to_str().unwrap()
-                    );
-                    Ok(shaderc::ResolvedInclude {
-                        resolved_name: String::from(name),
-                        content: glsl_code,
-                    })
-                }
-                Err(err) => Err(format!(
-                    "Failed to resolve include to {} in {} (was looking for {:?}): {}",
-                    name, source_file, path, err
-                )),
-            }
-        }
-        _ => Err(format!(
-            "Absolute include directives are not yet supported. Include to {} in {}.",
-            name, source_file
-        )),
+        Ok(Self { src, src_path })
     }
 }
 
 fn main() -> Result<()> {
     // Collect all shaders recursively within /src/
     let mut shader_paths = Vec::new();
-    shader_paths.extend(glob("./src/**/*.vert")?);
-    shader_paths.extend(glob("./src/**/*.frag")?);
-    shader_paths.extend(glob("./src/**/*.comp")?);
+    shader_paths.extend(glob("./src/**/*.wgsl")?);
 
     let shaders = shader_paths
         .into_par_iter()
@@ -87,30 +42,32 @@ fn main() -> Result<()> {
         .into_iter()
         .collect::<Result<Vec<_>>>()?;
 
-    let mut compiler = shaderc::Compiler::new().context("Unable to create shader compiler")?;
-    let mut options =
-        shaderc::CompileOptions::new().context("Unable to create shader compiler options")?;
-    options.set_warnings_as_errors();
-    options.set_generate_debug_info();
-    options.set_include_callback(include_directive_callback);
-
-    // Shader compilation cannot be parallelized. The [shaderc::Compiler] is not
-    // thread safe, and consumes significant resources.
-    // The best option would be to compile only what has changed
+    // Shader validation might be parallelized in the future.
+    // Ideally, only changed shaders would be validated.
+    let mut validator = naga::valid::Validator::new(naga::valid::ValidationFlags::all());
     for shader in shaders {
         println!(
             "cargo:rerun-if-changed={}",
             shader.src_path.as_os_str().to_str().unwrap()
         );
 
-        let compiled = compiler.compile_into_spirv(
-            &shader.src,
-            shader.kind,
-            &shader.src_path.to_str().unwrap(),
-            image_annealing_build_utils::SHADER_ENTRY_POINT,
-            Some(&options),
-        )?;
-        write(shader.spv_path, compiled.as_binary_u8())?;
+        let module = match naga::front::wgsl::parse_str(&shader.src) {
+            Ok(v) => v,
+            Err(ref e) => {
+                eprintln!("Unable to parse WGSL from {}:", shader.src_path.display());
+                print_err(e);
+                bail!("Unable to parse WGSL");
+            }
+        };
+
+        if let Err(ref error) = validator.validate(&module) {
+            eprintln!(
+                "Validation of WGSL from '{}' failed:",
+                shader.src_path.display()
+            );
+            print_err(error);
+            bail!("Validation of WGSL failed");
+        }
     }
 
     Ok(())
