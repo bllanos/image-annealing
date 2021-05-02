117323c0d90772785fcc7a6d5151f90dbbc71ae1
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2021-05-02 16:30:26 -0600

    Add permutation operation resources to resource manager

--- image_annealing/src/compute/resource/manager.rs
+++ image_annealing/src/compute/resource/manager.rs
@@ -1,18 +1,36 @@
-use super::buffer::PermutationOutputBuffer;
-use super::texture::PermutationOutputTexture;
+use super::buffer::{LosslessImageOutputBuffer, PermutationOutputBuffer};
+use super::texture::{
+    LosslessImageInputTexture, LosslessImageOutputTexture, PermutationInputTexture,
+    PermutationOutputTexture,
+};
 use crate::image_utils::ImageDimensions;
 
 pub struct ResourceManager {
-    permutation_output_buffer: PermutationOutputBuffer,
+    permutation_input_texture: PermutationInputTexture,
     permutation_output_texture: PermutationOutputTexture,
+    permutation_output_buffer: PermutationOutputBuffer,
+    lossless_image_input_texture: LosslessImageInputTexture,
+    lossless_image_output_texture: LosslessImageOutputTexture,
+    lossless_image_output_buffer: LosslessImageOutputBuffer,
 }
 
 impl ResourceManager {
     pub fn new(device: &wgpu::Device, image_dimensions: &ImageDimensions) -> Self {
         Self {
-            permutation_output_buffer: PermutationOutputBuffer::new(device, image_dimensions),
+            permutation_input_texture: PermutationInputTexture::new(device, image_dimensions),
             permutation_output_texture: PermutationOutputTexture::new(device, image_dimensions),
+            permutation_output_buffer: PermutationOutputBuffer::new(device, image_dimensions),
+            lossless_image_input_texture: LosslessImageInputTexture::new(device, image_dimensions),
+            lossless_image_output_texture: LosslessImageOutputTexture::new(
+                device,
+                image_dimensions,
+            ),
+            lossless_image_output_buffer: LosslessImageOutputBuffer::new(device, image_dimensions),
         }
+    }
+
+    pub fn permutation_input_texture(&self) -> &PermutationInputTexture {
+        &self.permutation_input_texture
     }
 
     pub fn permutation_output_texture(&self) -> &PermutationOutputTexture {
@@ -21,5 +39,17 @@ impl ResourceManager {
 
     pub fn permutation_output_buffer(&self) -> &PermutationOutputBuffer {
         &self.permutation_output_buffer
+    }
+
+    pub fn lossless_image_input_texture(&self) -> &LosslessImageInputTexture {
+        &self.lossless_image_input_texture
+    }
+
+    pub fn lossless_image_output_texture(&self) -> &LosslessImageOutputTexture {
+        &self.lossless_image_output_texture
+    }
+
+    pub fn lossless_image_output_buffer(&self) -> &LosslessImageOutputBuffer {
+        &self.lossless_image_output_buffer
     }
 }
