6061fce488d7da8f85701b639bacb1eb7f6692e8
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2021-07-04 20:07:14 -0600

    Allow OutputConfig to hold borrowed or owned values

--- image_annealing_shaders/src/output/mod.rs
+++ image_annealing_shaders/src/output/mod.rs
@@ -1,4 +1,5 @@
 use crate::shader;
+use std::borrow::Cow;
 use std::default::Default;
 use std::error::Error;
 use std::fmt;
@@ -32,7 +33,7 @@ impl Error for OutputDirectoryError {
 
 #[derive(Default)]
 pub struct OutputConfig<'a> {
-    create_permutation: Option<&'a Path>,
+    create_permutation: Option<Cow<'a, Path>>,
 }
 
 pub fn write_files(config: &OutputConfig) -> std::io::Result<()> {
@@ -51,7 +52,7 @@ pub fn write_default_files<P: AsRef<Path>>(directory: Option<P>) -> Result<(), B
     if path.exists() {
         if path.is_dir() {
             Ok(write_files(&OutputConfig {
-                create_permutation: Some(&path.join("create_permutation.wgsl")),
+                create_permutation: Some(Cow::from(path.join("create_permutation.wgsl"))),
             })?)
         } else {
             Err(Box::new(OutputDirectoryError::NotADirectory(
