49dda1f1d0420a5b0095eb9df64c8637f0e38d73
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2021-11-12 12:17:47 -0700

    Add texel swap operation tests for additional image dimensions

--- image_annealing/tests/swap/border/mod.rs
+++ image_annealing/tests/swap/border/mod.rs
@@ -1,16 +1,19 @@
 use image_annealing::compute;
 use image_annealing::compute::{OutputStatus, SwapInput, SwapParameters};
 use image_annealing::image_utils::validation::CandidatePermutation;
+use std::convert::TryInto;
 use std::error::Error;
 use test_utils::algorithm::assert_step_until_success;
 use test_utils::permutation::DimensionsAndPermutation;
 
-#[test]
-fn single_pixel() -> Result<(), Box<dyn Error>> {
+fn dimensions_wxh<T>(width: T, height: T) -> Result<(), Box<dyn Error>>
+where
+    T: TryInto<usize> + std::fmt::Debug + std::fmt::Display + Copy,
+{
     let DimensionsAndPermutation {
         permutation,
         dimensions,
-    } = test_utils::permutation::identity_with_dimensions(1, 1);
+    } = test_utils::permutation::identity_with_dimensions(width, height);
     let expected_permutation = test_utils::operation::swap(&permutation);
 
     let dispatcher = compute::create_dispatcher(&dimensions)?;
@@ -29,70 +32,36 @@ fn single_pixel() -> Result<(), Box<dyn Error>> {
 }
 
 #[test]
-fn even_even_dimensions() -> Result<(), Box<dyn Error>> {
-    let DimensionsAndPermutation {
-        permutation,
-        dimensions,
-    } = test_utils::permutation::identity_with_dimensions(2, 2);
-    let expected_permutation = test_utils::operation::swap(&permutation);
-
-    let dispatcher = compute::create_dispatcher(&dimensions)?;
-    let mut algorithm = dispatcher.swap(
-        SwapInput {
-            candidate_permutation: Some(CandidatePermutation(permutation.clone())),
-        },
-        SwapParameters {},
-    );
-    assert_step_until_success(algorithm.as_mut(), OutputStatus::FinalFullOutput)?;
-
-    let output = algorithm.full_output().unwrap();
-    assert_eq!(*output.input_permutation.unwrap().as_ref(), permutation);
-    assert_eq!(*output.output_permutation.as_ref(), expected_permutation);
-    Ok(())
+fn dimensions_1x1() -> Result<(), Box<dyn Error>> {
+    dimensions_wxh(1, 1)
 }
 
 #[test]
-fn even_odd_dimensions() -> Result<(), Box<dyn Error>> {
-    let DimensionsAndPermutation {
-        permutation,
-        dimensions,
-    } = test_utils::permutation::identity_with_dimensions(2, 1);
-    let expected_permutation = test_utils::operation::swap(&permutation);
-
-    let dispatcher = compute::create_dispatcher(&dimensions)?;
-    let mut algorithm = dispatcher.swap(
-        SwapInput {
-            candidate_permutation: Some(CandidatePermutation(permutation.clone())),
-        },
-        SwapParameters {},
-    );
-    assert_step_until_success(algorithm.as_mut(), OutputStatus::FinalFullOutput)?;
-
-    let output = algorithm.full_output().unwrap();
-    assert_eq!(*output.input_permutation.unwrap().as_ref(), permutation);
-    assert_eq!(*output.output_permutation.as_ref(), expected_permutation);
-    Ok(())
+fn dimensions_1x2() -> Result<(), Box<dyn Error>> {
+    dimensions_wxh(1, 2)
 }
 
 #[test]
-fn odd_even_dimensions() -> Result<(), Box<dyn Error>> {
-    let DimensionsAndPermutation {
-        permutation,
-        dimensions,
-    } = test_utils::permutation::identity_with_dimensions(1, 2);
-    let expected_permutation = test_utils::operation::swap(&permutation);
+fn dimensions_2x1() -> Result<(), Box<dyn Error>> {
+    dimensions_wxh(2, 1)
+}
 
-    let dispatcher = compute::create_dispatcher(&dimensions)?;
-    let mut algorithm = dispatcher.swap(
-        SwapInput {
-            candidate_permutation: Some(CandidatePermutation(permutation.clone())),
-        },
-        SwapParameters {},
-    );
-    assert_step_until_success(algorithm.as_mut(), OutputStatus::FinalFullOutput)?;
+#[test]
+fn dimensions_2x2() -> Result<(), Box<dyn Error>> {
+    dimensions_wxh(2, 2)
+}
 
-    let output = algorithm.full_output().unwrap();
-    assert_eq!(*output.input_permutation.unwrap().as_ref(), permutation);
-    assert_eq!(*output.output_permutation.as_ref(), expected_permutation);
-    Ok(())
+#[test]
+fn dimensions_2x3() -> Result<(), Box<dyn Error>> {
+    dimensions_wxh(2, 3)
+}
+
+#[test]
+fn dimensions_3x2() -> Result<(), Box<dyn Error>> {
+    dimensions_wxh(3, 2)
+}
+
+#[test]
+fn dimensions_3x3() -> Result<(), Box<dyn Error>> {
+    dimensions_wxh(3, 3)
 }
