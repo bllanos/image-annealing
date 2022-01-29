1c70ffa1110d3627e3c00e6183d95b166f65f66c
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2022-01-28 18:25:08 -0700

    Error reporting improvements to shader validation tests

--- image_annealing_shaders/tests/validate_shaders.rs
+++ image_annealing_shaders/tests/validate_shaders.rs
@@ -5,10 +5,17 @@ use image_annealing_shaders::shader;
 use std::error::Error;
 
 fn print_err(error: impl Error) {
-    eprintln!("\t{}:", error);
+    eprint!("{}", error);
+
     let mut e = error.source();
+    if e.is_some() {
+        eprintln!(": ");
+    } else {
+        eprintln!();
+    }
+
     while let Some(source) = e {
-        eprintln!("\t\t{}", source);
+        eprintln!("\t{}", source);
         e = source.source();
     }
 }
@@ -22,14 +29,14 @@ fn validate_shader(shader: &str) -> Result<(), Box<dyn Error>> {
     let module = match naga::front::wgsl::parse_str(shader) {
         Ok(v) => v,
         Err(e) => {
-            eprintln!("Unable to parse WGSL");
+            eprintln!("Unable to parse WGSL. Output the shader to a file and then run Naga's shader validation program for more information (https://github.com/gfx-rs/naga/blob/master/cli/src/main.rs).");
             print_err(&e);
             return Err(Box::new(e));
         }
     };
 
     if let Err(e) = validator.validate(&module) {
-        eprintln!("Validation of WGSL failed");
+        eprintln!("Validation of WGSL failed. Output the shader to a file and then run Naga's shader validation program for more information (https://github.com/gfx-rs/naga/blob/master/cli/src/main.rs).");
         print_err(&e);
         return Err(Box::new(e));
     }
