d66458cb88f535eb2c889f8a40e7fe58bc70b636
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2022-04-17 14:49:31 -0600

    Improve test for summing multiple elements
    - Swap stride needed to be accounted for in image dimensions

--- image_annealing/tests/swap/count/mod.rs
+++ image_annealing/tests/swap/count/mod.rs
@@ -1,5 +1,6 @@
 mod sum_at_least_two_elements_per_invocation {
     use super::super::border;
+    use image_annealing_shaders::constant::swap::STRIDE;
     use image_annealing_shaders::WorkgroupDimensions;
     use std::convert::TryInto;
     use std::error::Error;
@@ -8,6 +9,8 @@ mod sum_at_least_two_elements_per_invocation {
         let workgroup_dimensions = WorkgroupDimensions::count_swap();
         (workgroup_dimensions
             .invocation_count()
+            .checked_mul(STRIDE)
+            .unwrap()
             .checked_mul(3)
             .unwrap()
             .checked_sub(1)
@@ -18,21 +21,37 @@ mod sum_at_least_two_elements_per_invocation {
 
     #[test]
     fn long_accept_swap() -> Result<(), Box<dyn Error>> {
-        border::dimensions_wxh(at_least_two_elements_per_invocation(), 3, true)
+        border::dimensions_wxh(
+            at_least_two_elements_per_invocation(),
+            STRIDE.try_into()?,
+            true,
+        )
     }
 
     #[test]
     fn long_reject_swap() -> Result<(), Box<dyn Error>> {
-        border::dimensions_wxh(at_least_two_elements_per_invocation(), 3, false)
+        border::dimensions_wxh(
+            at_least_two_elements_per_invocation(),
+            STRIDE.try_into()?,
+            false,
+        )
     }
 
     #[test]
     fn tall_accept_swap() -> Result<(), Box<dyn Error>> {
-        border::dimensions_wxh(3, at_least_two_elements_per_invocation(), true)
+        border::dimensions_wxh(
+            STRIDE.try_into()?,
+            at_least_two_elements_per_invocation(),
+            true,
+        )
     }
 
     #[test]
     fn tall_reject_swap() -> Result<(), Box<dyn Error>> {
-        border::dimensions_wxh(3, at_least_two_elements_per_invocation(), false)
+        border::dimensions_wxh(
+            STRIDE.try_into()?,
+            at_least_two_elements_per_invocation(),
+            false,
+        )
     }
 }
