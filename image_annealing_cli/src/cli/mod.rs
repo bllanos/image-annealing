aea17368db6726c4399f34537912eb8e66fdd102
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2022-04-24 08:06:04 -0600

    Extract swap algorithm iteration into a separate function

--- image_annealing_cli/src/cli/mod.rs
+++ image_annealing_cli/src/cli/mod.rs
@@ -1,12 +1,14 @@
 use crate::config::{
-    AlgorithmConfig, Config, SwapParametersConfig, SwapStopConfig, SwapStopThreshold,
+    AlgorithmConfig, Config, DisplacementGoalPath, PermutationPath, SwapParametersConfig,
+    SwapStopConfig, SwapStopThreshold,
 };
 use image_annealing::compute::format::ImageFileWriter;
 use image_annealing::compute::{
-    self, CreatePermutationInput, CreatePermutationParameters, Dispatcher, OutputStatus,
-    PermuteInput, SwapInput, SwapParameters, SwapPartialOutput, SwapPassSelection,
-    ValidatePermutationInput, ValidatePermutationParameters,
+    self, CreatePermutationInput, CreatePermutationParameters, Dispatcher, PermuteInput, SwapInput,
+    SwapParameters, SwapPartialOutput, SwapPassSelection, ValidatePermutationInput,
+    ValidatePermutationParameters,
 };
+use image_annealing::{CandidatePermutation, DisplacementGoal, ValidatedPermutation};
 use std::error::Error;
 
 mod loader;
@@ -27,7 +29,7 @@ fn run_and_save(
         } => {
             let mut algorithm = dispatcher
                 .create_permutation(CreatePermutationInput {}, &CreatePermutationParameters {});
-            algorithm.step_until(OutputStatus::FinalFullOutput)?;
+            algorithm.step_until_finished()?;
             let permutation = algorithm.full_output().unwrap().validated_permutation;
             let output_path = permutation.save_add_extension(&path.0)?;
             println!("Wrote permutation to: {}", output_path.display());
@@ -46,7 +48,7 @@ fn run_and_save(
                 },
                 &Default::default(),
             );
-            algorithm.step_until(OutputStatus::FinalFullOutput)?;
+            algorithm.step_until_finished()?;
             let img = algorithm.full_output().unwrap().permuted_image;
             let output_path = img.save_add_extension(path.to_vec().as_slice())?;
             println!("Wrote permuted image to: {:?}", output_path);
@@ -54,92 +56,15 @@ fn run_and_save(
         AlgorithmConfig::Swap {
             candidate_permutation,
             displacement_goal,
-            permutation_output_path_no_extension: path,
-            parameters: SwapParametersConfig { stop },
-        } => {
-            let swap_pass_selection = SwapPassSelection::HORIZONTAL;
-            let (swap_parameters, threshold) = match stop {
-                SwapStopConfig::Bounded {
-                    threshold: Some(threshold_variant),
-                    ..
-                }
-                | SwapStopConfig::Unbounded(threshold_variant) => (
-                    SwapParameters::new(swap_pass_selection, true)?,
-                    Some(threshold_variant),
-                ),
-                _ => (SwapParameters::from_selection(swap_pass_selection)?, None),
-            };
-            let iteration_count = match stop {
-                SwapStopConfig::Bounded {
-                    iteration_count: count,
-                    ..
-                } => Some(count),
-                SwapStopConfig::Unbounded(_) => None,
-            };
-
-            let mut i = 0;
-            let mut algorithm_option = Some(dispatcher.swap(
-                SwapInput {
-                    candidate_permutation: Some(loader::load_candidate_permutation(
-                        candidate_permutation,
-                    )?),
-                    displacement_goal: Some(loader::load_displacement_goal(displacement_goal)?),
-                },
-                &swap_parameters,
-            ));
-            loop {
-                let mut algorithm = algorithm_option.take().unwrap();
-                algorithm.step_until_finished()?;
-
-                let mut stop = false;
-
-                if let Some(count) = iteration_count {
-                    if threshold.is_none() {
-                        println!("Texel swap round {}", i);
-                    }
-                    if i == count.get() {
-                        stop = true;
-                    }
-                }
-
-                if !stop {
-                    if let Some(threshold_variant) = threshold {
-                        let SwapPartialOutput {
-                            counts: swap_counts,
-                        } = algorithm.partial_output().unwrap();
-                        println!("Texel swap round {}, {}", i, swap_counts);
-                        match threshold_variant {
-                            SwapStopThreshold::SwapsAccepted(number_of_swaps) => {
-                                if swap_counts.accepted() <= *number_of_swaps {
-                                    stop = true;
-                                }
-                            }
-                            SwapStopThreshold::SwapAcceptanceFraction(fraction_of_swaps) => {
-                                if swap_counts.accepted_fraction() <= fraction_of_swaps.get() {
-                                    stop = true;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if stop {
-                    algorithm_option = Some(algorithm);
-                    break;
-                } else {
-                    let dispatcher = algorithm.return_to_dispatcher();
-                    algorithm_option = Some(dispatcher.swap(Default::default(), &swap_parameters));
-                    i += 1;
-                }
-            }
-            let permutation = algorithm_option
-                .unwrap()
-                .full_output()
-                .unwrap()
-                .output_permutation;
-            let output_path = permutation.save_add_extension(&path.0)?;
-            println!("Wrote swapped permutation to: {}", output_path.display());
-        }
+            permutation_output_path_no_extension,
+            parameters,
+        } => run_and_save_swap(
+            dispatcher,
+            candidate_permutation,
+            displacement_goal,
+            permutation_output_path_no_extension,
+            parameters,
+        )?,
         AlgorithmConfig::ValidatePermutation {
             candidate_permutation,
         } => {
@@ -151,7 +76,7 @@ fn run_and_save(
                 },
                 &ValidatePermutationParameters {},
             );
-            algorithm.step_until(OutputStatus::FinalFullOutput)?;
+            algorithm.step_until_finished()?;
             println!(
                 "Candidate permutation '{}' is valid",
                 candidate_permutation.0
@@ -159,4 +84,108 @@ fn run_and_save(
         }
     }
     Ok(())
+}
+
+fn run_and_save_swap(
+    dispatcher: Box<dyn Dispatcher>,
+    candidate_permutation: &PermutationPath,
+    displacement_goal: &DisplacementGoalPath,
+    permutation_output_path_no_extension: &PermutationPath,
+    parameters: &SwapParametersConfig,
+) -> Result<(), Box<dyn Error>> {
+    let permutation = run_swap(
+        dispatcher,
+        Some(loader::load_candidate_permutation(candidate_permutation)?),
+        Some(loader::load_displacement_goal(displacement_goal)?),
+        parameters,
+    )?;
+    let output_path = permutation.save_add_extension(&permutation_output_path_no_extension.0)?;
+    println!("Wrote swapped permutation to: {}", output_path.display());
+    Ok(())
+}
+
+fn run_swap(
+    dispatcher: Box<dyn Dispatcher>,
+    candidate_permutation: Option<CandidatePermutation>,
+    displacement_goal: Option<DisplacementGoal>,
+    parameters: &SwapParametersConfig,
+) -> Result<ValidatedPermutation, Box<dyn Error>> {
+    let swap_pass_selection = SwapPassSelection::HORIZONTAL;
+    let (swap_parameters, threshold) = match parameters.stop {
+        SwapStopConfig::Bounded {
+            threshold: Some(threshold_variant),
+            ..
+        }
+        | SwapStopConfig::Unbounded(threshold_variant) => (
+            SwapParameters::new(swap_pass_selection, true)?,
+            Some(threshold_variant),
+        ),
+        _ => (SwapParameters::from_selection(swap_pass_selection)?, None),
+    };
+    let iteration_count = match parameters.stop {
+        SwapStopConfig::Bounded {
+            iteration_count: count,
+            ..
+        } => Some(count),
+        SwapStopConfig::Unbounded(_) => None,
+    };
+
+    let mut i = 0;
+    let mut algorithm_option = Some(dispatcher.swap(
+        SwapInput {
+            candidate_permutation,
+            displacement_goal,
+        },
+        &swap_parameters,
+    ));
+    loop {
+        let mut algorithm = algorithm_option.take().unwrap();
+        algorithm.step_until_finished()?;
+
+        let mut stop = false;
+
+        if let Some(count) = iteration_count {
+            if threshold.is_none() {
+                println!("Texel swap round {}", i);
+            }
+            if i == count.get() {
+                stop = true;
+            }
+        }
+
+        if !stop {
+            if let Some(threshold_variant) = threshold {
+                let SwapPartialOutput {
+                    counts: swap_counts,
+                } = algorithm.partial_output().unwrap();
+                println!("Texel swap round {}, {}", i, swap_counts);
+                match threshold_variant {
+                    SwapStopThreshold::SwapsAccepted(number_of_swaps) => {
+                        if swap_counts.accepted() <= number_of_swaps {
+                            stop = true;
+                        }
+                    }
+                    SwapStopThreshold::SwapAcceptanceFraction(fraction_of_swaps) => {
+                        if swap_counts.accepted_fraction() <= fraction_of_swaps.get() {
+                            stop = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        if stop {
+            algorithm_option = Some(algorithm);
+            break;
+        } else {
+            let dispatcher = algorithm.return_to_dispatcher();
+            algorithm_option = Some(dispatcher.swap(Default::default(), &swap_parameters));
+            i += 1;
+        }
+    }
+    Ok(algorithm_option
+        .unwrap()
+        .full_output()
+        .unwrap()
+        .output_permutation)
 }
