f2fb7bba674851dbd49256ec3da7568db943b3c6
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2022-05-08 17:04:47 -0600

    Additional unit tests for DisplacementGoal

--- image_annealing/src/image_utils/displacement_goal/tests.rs
+++ image_annealing/src/image_utils/displacement_goal/tests.rs
@@ -1,9 +1,20 @@
 mod displacement_goal {
     use super::super::validation;
     use super::super::DisplacementGoal;
-    use crate::CandidatePermutation;
+    use crate::compute::format::Rgba8Image;
+    use crate::{CandidatePermutation, ImageDimensions, ImageDimensionsHolder};
     use std::error::Error;
     use test_utils::permutation::{self, DimensionsAndPermutation};
+
+    #[test]
+    fn from_vector_field_image() -> Result<(), Box<dyn Error>> {
+        let DimensionsAndPermutation { permutation, .. } = permutation::non_identity();
+        let expected = permutation.clone();
+        let displacement_goal =
+            DisplacementGoal::from_vector_field_image(Rgba8Image::new(permutation)?);
+        assert_eq!(*displacement_goal.as_ref(), expected);
+        Ok(())
+    }
 
     #[test]
     fn from_vector_field() -> Result<(), Box<dyn Error>> {
@@ -11,6 +22,15 @@ mod displacement_goal {
         let expected = permutation.clone();
         let displacement_goal = DisplacementGoal::from_vector_field(permutation)?;
         assert_eq!(*displacement_goal.as_ref(), expected);
+        Ok(())
+    }
+
+    #[test]
+    fn from_raw_candidate_permutation() -> Result<(), Box<dyn Error>> {
+        let DimensionsAndPermutation { permutation, .. } = permutation::non_identity();
+        let expected = validation::validate_permutation(permutation.clone())?.inverse();
+        let displacement_goal = DisplacementGoal::from_raw_candidate_permutation(permutation)?;
+        assert_eq!(displacement_goal.as_ref(), expected.as_ref());
         Ok(())
     }
 
@@ -32,5 +52,94 @@ mod displacement_goal {
         let displacement_goal = DisplacementGoal::from(validated_permutation);
         assert_eq!(displacement_goal.as_ref(), expected.as_ref());
         Ok(())
+    }
+
+    #[test]
+    fn into_inner() -> Result<(), Box<dyn Error>> {
+        let DimensionsAndPermutation { permutation, .. } = permutation::non_identity();
+        let expected = permutation.clone();
+        let displacement_goal = DisplacementGoal::from_vector_field(permutation)?;
+        assert_eq!(displacement_goal.into_inner(), expected);
+        Ok(())
+    }
+
+    #[test]
+    fn as_raw_slice() -> Result<(), Box<dyn Error>> {
+        let DimensionsAndPermutation { permutation, .. } = permutation::non_identity();
+        let expected = permutation.clone();
+        let displacement_goal = DisplacementGoal::from_vector_field(permutation)?;
+        assert_eq!(
+            displacement_goal.as_raw_slice(),
+            expected.as_raw().as_slice()
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn dimensions() -> Result<(), Box<dyn Error>> {
+        let DimensionsAndPermutation { permutation, .. } = permutation::non_identity();
+        let expected = ImageDimensions::from_image(&permutation)?;
+        let displacement_goal = DisplacementGoal::from_vector_field(permutation)?;
+        assert_eq!(*displacement_goal.dimensions(), expected);
+        Ok(())
+    }
+
+    mod io {
+        use super::super::super::DisplacementGoal;
+        use crate::compute::format::{ImageFileReader, ImageFileWriter, VectorFieldImageBuffer};
+        use std::error::Error;
+        use test_utils::permutation::{self, DimensionsAndPermutation};
+
+        #[test]
+        fn success() -> Result<(), Box<dyn Error>> {
+            let DimensionsAndPermutation { permutation, .. } = permutation::non_identity();
+            let expected = permutation.clone();
+            let displacement_goal = DisplacementGoal::from_vector_field(permutation)?;
+            assert_eq!(*displacement_goal.as_ref(), expected);
+
+            let path = test_utils::make_test_output_path_string(&[
+                "image_utils_displacement_goal_io_image",
+            ]);
+            let expected_output_path = VectorFieldImageBuffer::make_filename(&path);
+            if expected_output_path.is_file() {
+                panic!("Image already exists in the filesystem")
+            }
+
+            let full_output_path = displacement_goal.save_add_extension(path)?;
+            assert_eq!(full_output_path, expected_output_path);
+
+            let read_image = VectorFieldImageBuffer::load(&full_output_path)?;
+            assert_eq!(read_image, expected);
+
+            let input_displacement_goal = DisplacementGoal::load(&full_output_path)?;
+            assert_eq!(*input_displacement_goal.as_ref(), expected);
+
+            assert_eq!(input_displacement_goal, displacement_goal);
+
+            Ok(std::fs::remove_file(expected_output_path)?)
+        }
+
+        #[test]
+        fn load_missing_image() {
+            let path = test_utils::make_test_data_path(&["image", "image", "not_found.png"]);
+            test_utils::assert_error_contains(
+                DisplacementGoal::load(path),
+                "No such file or directory",
+            );
+        }
+
+        #[test]
+        fn save_missing_directory() -> Result<(), Box<dyn Error>> {
+            let DimensionsAndPermutation { permutation, .. } = permutation::non_identity();
+            let displacement_goal = DisplacementGoal::from_vector_field(permutation)?;
+            test_utils::assert_error_contains(
+                displacement_goal.save_add_extension(&test_utils::make_test_output_path(&[
+                    "not_found",
+                    "cannot_create",
+                ])),
+                "No such file or directory",
+            );
+            Ok(())
+        }
     }
 }
