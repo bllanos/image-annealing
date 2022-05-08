7a5548c58a657295d529bea7695d09f80fe9ec8a
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2022-05-08 16:14:27 -0600

    Additional unit tests for ImageDimensions

--- image_annealing/src/image_utils/dimension/tests.rs
+++ image_annealing/src/image_utils/dimension/tests.rs
@@ -3,39 +3,44 @@ use image::RgbaImage;
 use std::convert::TryInto;
 use std::error::Error;
 
-#[test]
-fn negative_width() {
-    test_utils::assert_error_contains(
-        ImageDimensions::new(-1, 1),
-        "failed to convert -1 to the required type for dimensions",
-    )
-}
+mod new {
+    use super::super::ImageDimensions;
+    use std::error::Error;
 
-#[test]
-fn negative_height() {
-    test_utils::assert_error_contains(
-        ImageDimensions::new(1, -1),
-        "failed to convert -1 to the required type for dimensions",
-    )
-}
+    #[test]
+    fn negative_width() {
+        test_utils::assert_error_contains(
+            ImageDimensions::new(-1, 1),
+            "failed to convert -1 to the required type for dimensions",
+        )
+    }
 
-#[test]
-fn zero_width() {
-    test_utils::assert_error_contains(ImageDimensions::new(0, 1), "width is zero")
-}
+    #[test]
+    fn negative_height() {
+        test_utils::assert_error_contains(
+            ImageDimensions::new(1, -1),
+            "failed to convert -1 to the required type for dimensions",
+        )
+    }
 
-#[test]
-fn zero_height() {
-    test_utils::assert_error_contains(ImageDimensions::new(1, 0), "height is zero")
-}
+    #[test]
+    fn zero_width() {
+        test_utils::assert_error_contains(ImageDimensions::new(0, 1), "width is zero")
+    }
 
-#[test]
-fn valid_dimensions() -> Result<(), Box<dyn Error>> {
-    let dim = ImageDimensions::new(63, 64)?;
-    assert_eq!(dim.width(), 63);
-    assert_eq!(dim.height(), 64);
-    assert_eq!(dim.count(), 63 * 64);
-    Ok(())
+    #[test]
+    fn zero_height() {
+        test_utils::assert_error_contains(ImageDimensions::new(1, 0), "height is zero")
+    }
+
+    #[test]
+    fn valid_dimensions() -> Result<(), Box<dyn Error>> {
+        let dim = ImageDimensions::new(63, 64)?;
+        assert_eq!(dim.width(), 63);
+        assert_eq!(dim.height(), 64);
+        assert_eq!(dim.count(), 63 * 64);
+        Ok(())
+    }
 }
 
 #[test]
@@ -45,6 +50,29 @@ fn from_image() -> Result<(), Box<dyn Error>> {
     assert_eq!(dim.width(), image.width().try_into().unwrap());
     assert_eq!(dim.height(), image.height().try_into().unwrap());
     Ok(())
+}
+
+mod from_image_path {
+    use super::ImageDimensions;
+    use std::error::Error;
+
+    #[test]
+    fn from_image_path() -> Result<(), Box<dyn Error>> {
+        let path = test_utils::make_test_data_path(&["image", "image", "stripes.png"]);
+        let dim = ImageDimensions::from_image_path(path)?;
+        assert_eq!(dim.width(), 20);
+        assert_eq!(dim.height(), 25);
+        Ok(())
+    }
+
+    #[test]
+    fn missing_image() {
+        let path = test_utils::make_test_data_path(&["image", "image", "not_found.png"]);
+        test_utils::assert_error_contains(
+            ImageDimensions::from_image_path(path),
+            "No such file or directory",
+        );
+    }
 }
 
 mod make_linear_index {
@@ -156,6 +184,156 @@ mod make_coordinates {
         coordinates = dim.make_coordinates(14)?;
         assert_eq!(coordinates.0, 2);
         assert_eq!(coordinates.1, 4);
+        Ok(())
+    }
+}
+
+#[test]
+fn to_extent() -> Result<(), Box<dyn Error>> {
+    let dim = ImageDimensions::new(63, 64)?;
+    assert_eq!(
+        dim.to_extent(),
+        wgpu::Extent3d {
+            width: 63,
+            height: 64,
+            depth_or_array_layers: 1
+        }
+    );
+    Ok(())
+}
+
+mod try_from_extent {
+    use super::super::ImageDimensions;
+    use std::convert::TryFrom;
+    use std::error::Error;
+
+    #[test]
+    fn zero_depth() {
+        test_utils::assert_error_contains(
+            <ImageDimensions as TryFrom<wgpu::Extent3d>>::try_from(wgpu::Extent3d {
+                width: 63,
+                height: 64,
+                depth_or_array_layers: 0,
+            }),
+            "depth is not 1",
+        )
+    }
+
+    #[test]
+    fn zero_width() {
+        test_utils::assert_error_contains(
+            <ImageDimensions as TryFrom<wgpu::Extent3d>>::try_from(wgpu::Extent3d {
+                width: 0,
+                height: 64,
+                depth_or_array_layers: 1,
+            }),
+            "width is zero",
+        )
+    }
+
+    #[test]
+    fn zero_height() {
+        test_utils::assert_error_contains(
+            <ImageDimensions as TryFrom<wgpu::Extent3d>>::try_from(wgpu::Extent3d {
+                width: 63,
+                height: 0,
+                depth_or_array_layers: 1,
+            }),
+            "height is zero",
+        )
+    }
+
+    #[test]
+    fn large_depth() {
+        test_utils::assert_error_contains(
+            <ImageDimensions as TryFrom<wgpu::Extent3d>>::try_from(wgpu::Extent3d {
+                width: 63,
+                height: 64,
+                depth_or_array_layers: 2,
+            }),
+            "depth is not 1",
+        )
+    }
+
+    #[test]
+    fn valid_dimensions() -> Result<(), Box<dyn Error>> {
+        let dim = <ImageDimensions as TryFrom<wgpu::Extent3d>>::try_from(wgpu::Extent3d {
+            width: 63,
+            height: 64,
+            depth_or_array_layers: 1,
+        })?;
+        assert_eq!(dim.width(), 63);
+        assert_eq!(dim.height(), 64);
+        Ok(())
+    }
+}
+
+mod partial_eq_extent {
+    use super::super::ImageDimensions;
+    use std::error::Error;
+
+    #[test]
+    fn zero_depth() -> Result<(), Box<dyn Error>> {
+        let dim = ImageDimensions::new(63, 64)?;
+        assert!(
+            dim != wgpu::Extent3d {
+                width: 63,
+                height: 64,
+                depth_or_array_layers: 0,
+            }
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn large_depth() -> Result<(), Box<dyn Error>> {
+        let dim = ImageDimensions::new(63, 64)?;
+        assert!(
+            dim != wgpu::Extent3d {
+                width: 63,
+                height: 64,
+                depth_or_array_layers: 2,
+            }
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn different_width() -> Result<(), Box<dyn Error>> {
+        let dim = ImageDimensions::new(63, 64)?;
+        assert!(
+            dim != wgpu::Extent3d {
+                width: 64,
+                height: 64,
+                depth_or_array_layers: 1,
+            }
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn different_height() -> Result<(), Box<dyn Error>> {
+        let dim = ImageDimensions::new(63, 64)?;
+        assert!(
+            dim != wgpu::Extent3d {
+                width: 63,
+                height: 63,
+                depth_or_array_layers: 1,
+            }
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn equal() -> Result<(), Box<dyn Error>> {
+        let dim = ImageDimensions::new(63, 64)?;
+        assert!(
+            dim == wgpu::Extent3d {
+                width: 63,
+                height: 64,
+                depth_or_array_layers: 1,
+            }
+        );
         Ok(())
     }
 }
