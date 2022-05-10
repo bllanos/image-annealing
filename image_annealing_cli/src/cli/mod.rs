7d5e097f7744a9c6de0e2e1aa2467e529f0122b1
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2022-05-10 17:30:16 -0600

    Extract swap operation from the main CLI module to a new module

--- image_annealing_cli/src/cli/mod.rs
+++ image_annealing_cli/src/cli/mod.rs
@@ -1,17 +1,13 @@
-use crate::config::{
-    AlgorithmConfig, Config, DisplacementGoalPath, PermutationPath, SwapParametersConfig,
-    SwapStopConfig, SwapStopThreshold,
-};
+use crate::config::{AlgorithmConfig, Config};
 use image_annealing::compute::format::ImageFileWriter;
 use image_annealing::compute::{
-    self, CreatePermutationInput, CreatePermutationParameters, Dispatcher, PermuteInput, SwapInput,
-    SwapParameters, SwapPartialOutput, SwapPassSelection, ValidatePermutationInput,
-    ValidatePermutationParameters,
+    self, CreatePermutationInput, CreatePermutationParameters, Dispatcher, PermuteInput,
+    ValidatePermutationInput, ValidatePermutationParameters,
 };
-use image_annealing::{CandidatePermutation, DisplacementGoal, ValidatedPermutation};
 use std::error::Error;
 
 mod loader;
+mod swap;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let dispatcher = compute::create_dispatcher(&config.dispatcher)?;
@@ -58,7 +54,7 @@ fn run_and_save(
             displacement_goal,
             permutation_output_path_no_extension,
             parameters,
-        } => run_and_save_swap(
+        } => swap::run_and_save_swap(
             dispatcher,
             candidate_permutation,
             displacement_goal,
@@ -84,109 +80,4 @@ fn run_and_save(
         }
     }
     Ok(())
-}
-
-fn run_and_save_swap(
-    dispatcher: Box<dyn Dispatcher>,
-    candidate_permutation: &PermutationPath,
-    displacement_goal: &DisplacementGoalPath,
-    permutation_output_path_no_extension: &PermutationPath,
-    parameters: &SwapParametersConfig,
-) -> Result<(), Box<dyn Error>> {
-    let permutation = run_swap(
-        dispatcher,
-        Some(loader::load_candidate_permutation(candidate_permutation)?),
-        Some(loader::load_displacement_goal(displacement_goal)?),
-        parameters,
-    )?;
-    let output_path = permutation.save_add_extension(&permutation_output_path_no_extension.0)?;
-    println!("Wrote swapped permutation to: {}", output_path.display());
-    Ok(())
-}
-
-fn run_swap(
-    dispatcher: Box<dyn Dispatcher>,
-    candidate_permutation: Option<CandidatePermutation>,
-    displacement_goal: Option<DisplacementGoal>,
-    parameters: &SwapParametersConfig,
-) -> Result<ValidatedPermutation, Box<dyn Error>> {
-    // TODO Input the SwapPassSelection from the SwapParametersConfig
-    let swap_pass_selection = SwapPassSelection::HORIZONTAL;
-    let (swap_parameters, threshold) = match parameters.stop {
-        SwapStopConfig::Bounded {
-            threshold: Some(threshold_variant),
-            ..
-        }
-        | SwapStopConfig::Unbounded(threshold_variant) => (
-            SwapParameters::new(swap_pass_selection, true)?,
-            Some(threshold_variant),
-        ),
-        _ => (SwapParameters::from_selection(swap_pass_selection)?, None),
-    };
-    let iteration_count = match parameters.stop {
-        SwapStopConfig::Bounded {
-            iteration_count: count,
-            ..
-        } => Some(count),
-        SwapStopConfig::Unbounded(_) => None,
-    };
-
-    let mut i = 0;
-    let mut algorithm_option = Some(dispatcher.swap(
-        SwapInput {
-            candidate_permutation,
-            displacement_goal,
-        },
-        &swap_parameters,
-    ));
-    loop {
-        let mut algorithm = algorithm_option.take().unwrap();
-        algorithm.step_until_finished()?;
-
-        let mut stop = false;
-
-        if let Some(count) = iteration_count {
-            if threshold.is_none() {
-                println!("Texel swap round {}", i);
-            }
-            if i == count.get() {
-                stop = true;
-            }
-        }
-
-        if !stop {
-            if let Some(threshold_variant) = threshold {
-                let SwapPartialOutput {
-                    counts: swap_counts,
-                } = algorithm.partial_output().unwrap();
-                println!("Texel swap round {}, {}", i, swap_counts);
-                match threshold_variant {
-                    SwapStopThreshold::SwapsAccepted(number_of_swaps) => {
-                        if swap_counts.accepted() <= number_of_swaps {
-                            stop = true;
-                        }
-                    }
-                    SwapStopThreshold::SwapAcceptanceFraction(fraction_of_swaps) => {
-                        if swap_counts.accepted_fraction() <= fraction_of_swaps.get() {
-                            stop = true;
-                        }
-                    }
-                }
-            }
-        }
-
-        if stop {
-            algorithm_option = Some(algorithm);
-            break;
-        } else {
-            let dispatcher = algorithm.return_to_dispatcher();
-            algorithm_option = Some(dispatcher.swap(Default::default(), &swap_parameters));
-            i += 1;
-        }
-    }
-    Ok(algorithm_option
-        .unwrap()
-        .full_output()
-        .unwrap()
-        .output_permutation)
 }
---
+++ image_annealing_cli/src/cli/swap/mod.rs
@@ -0,0 +1,115 @@
+use super::loader;
+use crate::config::{
+    DisplacementGoalPath, PermutationPath, SwapParametersConfig, SwapStopConfig, SwapStopThreshold,
+};
+use image_annealing::compute::format::ImageFileWriter;
+use image_annealing::compute::{
+    Dispatcher, SwapInput, SwapParameters, SwapPartialOutput, SwapPassSelection,
+};
+use image_annealing::{CandidatePermutation, DisplacementGoal, ValidatedPermutation};
+use std::error::Error;
+
+pub fn run_and_save_swap(
+    dispatcher: Box<dyn Dispatcher>,
+    candidate_permutation: &PermutationPath,
+    displacement_goal: &DisplacementGoalPath,
+    permutation_output_path_no_extension: &PermutationPath,
+    parameters: &SwapParametersConfig,
+) -> Result<(), Box<dyn Error>> {
+    let permutation = run_swap(
+        dispatcher,
+        Some(loader::load_candidate_permutation(candidate_permutation)?),
+        Some(loader::load_displacement_goal(displacement_goal)?),
+        parameters,
+    )?;
+    let output_path = permutation.save_add_extension(&permutation_output_path_no_extension.0)?;
+    println!("Wrote swapped permutation to: {}", output_path.display());
+    Ok(())
+}
+
+fn run_swap(
+    dispatcher: Box<dyn Dispatcher>,
+    candidate_permutation: Option<CandidatePermutation>,
+    displacement_goal: Option<DisplacementGoal>,
+    parameters: &SwapParametersConfig,
+) -> Result<ValidatedPermutation, Box<dyn Error>> {
+    // TODO Input the SwapPassSelection from the SwapParametersConfig
+    let swap_pass_selection = SwapPassSelection::HORIZONTAL;
+    let (swap_parameters, threshold) = match parameters.stop {
+        SwapStopConfig::Bounded {
+            threshold: Some(threshold_variant),
+            ..
+        }
+        | SwapStopConfig::Unbounded(threshold_variant) => (
+            SwapParameters::new(swap_pass_selection, true)?,
+            Some(threshold_variant),
+        ),
+        _ => (SwapParameters::from_selection(swap_pass_selection)?, None),
+    };
+    let iteration_count = match parameters.stop {
+        SwapStopConfig::Bounded {
+            iteration_count: count,
+            ..
+        } => Some(count),
+        SwapStopConfig::Unbounded(_) => None,
+    };
+
+    let mut i = 0;
+    let mut algorithm_option = Some(dispatcher.swap(
+        SwapInput {
+            candidate_permutation,
+            displacement_goal,
+        },
+        &swap_parameters,
+    ));
+    loop {
+        let mut algorithm = algorithm_option.take().unwrap();
+        algorithm.step_until_finished()?;
+
+        let mut stop = false;
+
+        if let Some(count) = iteration_count {
+            if threshold.is_none() {
+                println!("Texel swap round {}", i);
+            }
+            if i == count.get() {
+                stop = true;
+            }
+        }
+
+        if !stop {
+            if let Some(threshold_variant) = threshold {
+                let SwapPartialOutput {
+                    counts: swap_counts,
+                } = algorithm.partial_output().unwrap();
+                println!("Texel swap round {}, {}", i, swap_counts);
+                match threshold_variant {
+                    SwapStopThreshold::SwapsAccepted(number_of_swaps) => {
+                        if swap_counts.accepted() <= number_of_swaps {
+                            stop = true;
+                        }
+                    }
+                    SwapStopThreshold::SwapAcceptanceFraction(fraction_of_swaps) => {
+                        if swap_counts.accepted_fraction() <= fraction_of_swaps.get() {
+                            stop = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        if stop {
+            algorithm_option = Some(algorithm);
+            break;
+        } else {
+            let dispatcher = algorithm.return_to_dispatcher();
+            algorithm_option = Some(dispatcher.swap(Default::default(), &swap_parameters));
+            i += 1;
+        }
+    }
+    Ok(algorithm_option
+        .unwrap()
+        .full_output()
+        .unwrap()
+        .output_permutation)
+}
