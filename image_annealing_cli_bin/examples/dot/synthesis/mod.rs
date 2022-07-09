132dcb72e1282b8d0bd9da7129980d6fbc0b1462
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2022-07-09 17:25:22 -0600

    Code documentation for the dot example

--- image_annealing_cli_bin/examples/dot/synthesis/mod.rs
+++ image_annealing_cli_bin/examples/dot/synthesis/mod.rs
@@ -1,15 +1,27 @@
+//! # Example data synthesis
+//!
+//! Input data generation functions for the 'dot' example
+
 use image_annealing::compute::conversion::{VectorFieldEntry, VectorFieldEntryComponent};
 use image_annealing::compute::format::Rgba8Image;
 use image_annealing::{DisplacementGoal, ImageDimensions};
 
+/// The geometrical parameters of a dot (a disc)
 #[derive(Debug, PartialEq)]
 struct DotGeometry {
+    /// Squared radius of the dot
     radius_squared: f64,
+    /// x-coordinate of the center of the dot
     x: f64,
+    /// y-coordinate of the center of the dot
     y: f64,
 }
 
 impl DotGeometry {
+    /// Find the parameters of a dot centered in an image with the given dimensions
+    ///
+    /// The dot just fits inside the image borders.
+    /// The radius of the dot is the floor of half the smaller of the image width and height.
     pub fn new(dimensions: &ImageDimensions) -> Self {
         let min_dimension = std::cmp::min(dimensions.width(), dimensions.height());
         let radius = min_dimension / 2;
@@ -21,6 +33,9 @@ impl DotGeometry {
     }
 }
 
+/// Create an image of a white dot on a black background
+///
+/// The dot's geometrical parameters are created using [`DotGeometry::new(dimensions)`](DotGeometry::new)
 pub fn white_dot(dimensions: &ImageDimensions) -> Rgba8Image {
     let DotGeometry {
         radius_squared,
@@ -46,6 +61,11 @@ pub fn white_dot(dimensions: &ImageDimensions) -> Rgba8Image {
     Rgba8Image::new(image).unwrap()
 }
 
+/// Create a displacement goal where a disc-shaped region in the center of the image
+/// wants to move directly away from the center, whereas the surrounding regions
+/// want to move towards the center
+///
+/// The disc's geometrical parameters are created using [`DotGeometry::new(dimensions)`](DotGeometry::new)
 pub fn dot_goal(dimensions: &ImageDimensions) -> DisplacementGoal {
     let DotGeometry {
         radius_squared,
@@ -66,9 +86,13 @@ pub fn dot_goal(dimensions: &ImageDimensions) -> DisplacementGoal {
             );
             let distance_squared = vector_from_center.0.powi(2) + vector_from_center.1.powi(2);
             if distance_squared > radius_squared {
+                // Pixel is outside the disc
                 VectorFieldEntry(-vector_from_center_integer.0, -vector_from_center_integer.1)
                     .to_pixel()
             } else {
+                // Pixel is inside the disc
+                //
+                // Create a vector that points as far as possible away from the center of the image
                 let max_component = std::cmp::max(
                     vector_from_center_integer.0.abs(),
                     vector_from_center_integer.1.abs(),
@@ -79,6 +103,8 @@ pub fn dot_goal(dimensions: &ImageDimensions) -> DisplacementGoal {
                         VectorFieldEntryComponent::MAX,
                     )
                 } else {
+                    // Scale the vector such that it points as far as possible away from the center
+                    // of the image
                     let scale = VectorFieldEntryComponent::MAX / max_component;
                     VectorFieldEntry(
                         scale * vector_from_center_integer.0,
