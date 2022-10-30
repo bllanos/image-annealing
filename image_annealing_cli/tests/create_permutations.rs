80f1cbb79cce619717c4d470b7118d6bd75844b4
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2022-10-30 15:27:32 -0600

    Test dimensions of identity permutation

--- image_annealing_cli/tests/create_permutations.rs
+++ image_annealing_cli/tests/create_permutations.rs
@@ -1,5 +1,5 @@
 use image_annealing::compute::format::{ImageFileReader, ImageFileWriter, VectorFieldImageBuffer};
-use image_annealing::{compute, CandidatePermutation, ImageDimensions};
+use image_annealing::{compute, CandidatePermutation, ImageDimensions, ImageDimensionsHolder};
 use image_annealing_cli::cli;
 use image_annealing_cli::config::{AlgorithmConfig, Config, ImagePath, PermutationPath};
 use std::error::Error;
@@ -11,18 +11,20 @@ fn create_permutation() -> Result<(), Box<dyn Error>> {
     let full_output_path = VectorFieldImageBuffer::make_filename(&path);
     assert!(!full_output_path.is_file());
 
+    let dimensions = ImageDimensions::new(3, 4)?;
     let config = Config {
         algorithm: AlgorithmConfig::CreatePermutation {
             permutation_output_path_no_extension: PermutationPath::from_raw(path),
         },
         dispatcher: compute::Config {
-            image_dimensions: ImageDimensions::new(3, 4)?,
+            image_dimensions: dimensions,
         },
     };
     cli::run(config)?;
 
     let output_permutation = CandidatePermutation::load(&full_output_path)?;
     permutation::assert_is_identity(&output_permutation);
+    assert_eq!(output_permutation.dimensions(), &dimensions);
     std::fs::remove_file(full_output_path)?;
 
     Ok(())
