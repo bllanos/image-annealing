0736b56fed3bcf1902c6be47cace94906d8f3d0d
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2022-12-04 18:47:52 -0700

    Remove functions from displacement goal shader preamble
    - Not all users may want these functions

--- image_annealing_shader/src/shader/create_displacement_goal/mod.rs
+++ image_annealing_shader/src/shader/create_displacement_goal/mod.rs
@@ -4,24 +4,20 @@ use crate::function::{conversion, io, main};
 use std::borrow::Cow;
 use std::io::Write;
 
-fn create_displacement_goal_common<W: Write>(mut writer: W) -> std::io::Result<()> {
-    binding::create_displacement_goal::bind_group(&mut writer)?;
-    conversion::i32_to_u16(&mut writer)?;
-    io::store_displacement_goal_vector(&mut writer)
-}
-
-fn create_displacement_goal_preamble<W: Write>(mut writer: W) -> std::io::Result<()> {
+fn create_displacement_goal_resources<W: Write>(mut writer: W) -> std::io::Result<()> {
     writeln!(
         writer,
-        "// Begin preamble provided by {}",
+        "// Begin resources provided by {}",
         crate::crate_name()
     )?;
-    create_displacement_goal_common(&mut writer)?;
-    writeln!(writer, "// End preamble")
+    binding::create_displacement_goal::bind_group(&mut writer)?;
+    writeln!(writer, "// End resources")
 }
 
 pub fn create_displacement_goal_default<W: Write>(mut writer: W) -> std::io::Result<()> {
-    create_displacement_goal_common(&mut writer)?;
+    create_displacement_goal_resources(&mut writer)?;
+    conversion::i32_to_u16(&mut writer)?;
+    io::store_displacement_goal_vector(&mut writer)?;
     compute::compute_shader_annotation(
         &mut writer,
         WorkgroupDimensions::create_displacement_goal_default(),
@@ -38,7 +34,7 @@ pub fn create_displacement_goal_custom<W: Write>(
     mut writer: W,
     content: &CreateDisplacementGoalShaderContent,
 ) -> std::io::Result<()> {
-    create_displacement_goal_preamble(&mut writer)?;
+    create_displacement_goal_resources(&mut writer)?;
     writer.write_all(content.body.as_bytes())?;
     writeln!(writer)
 }
