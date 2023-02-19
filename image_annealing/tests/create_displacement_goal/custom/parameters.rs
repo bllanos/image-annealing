4cd15442a2f682b5bd3f01eb270280d7f3732525
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2023-02-19 12:20:46 -0700

    Refactor existing custom displacement goal generation test
    - Extract helper function
    - Move to a new file

---
+++ image_annealing/tests/create_displacement_goal/custom/parameters.rs
@@ -0,0 +1,70 @@
+use image_annealing::compute::format::VectorFieldImageBuffer;
+use image_annealing::compute::{
+    self, Config, CreateDisplacementGoalParameters, CreateDisplacementGoalPipelineOperation,
+    CreateDisplacementGoalShaderConfig, OutputStatus, PipelineConfig, WorkgroupGridConfig,
+};
+use image_annealing::{DisplacementGoal, ImageDimensions};
+use std::borrow::Cow;
+use std::default::Default;
+use std::error::Error;
+use std::num::NonZeroU32;
+use test_util::algorithm::assert_step_until_success;
+
+const CUSTOM_SHADER_ENTRY_POINT: &str = "entry_point";
+
+fn make_filled_rectangle_displacement_goal(
+    image_dimensions: &ImageDimensions,
+    rectangle_dimensions: &ImageDimensions,
+) -> DisplacementGoal {
+    let mut image = VectorFieldImageBuffer::from_pixel(
+        image_dimensions.width().try_into().unwrap(),
+        image_dimensions.height().try_into().unwrap(),
+        image::Rgba([0; 4]),
+    );
+    let rectangle_width = rectangle_dimensions.width().try_into().unwrap();
+    let rectangle_height = rectangle_dimensions.height().try_into().unwrap();
+    for (x, y, px) in image.enumerate_pixels_mut() {
+        if x < rectangle_width && y < rectangle_height {
+            *px = image::Rgba([1, 2, 3, 4]);
+        }
+    }
+    DisplacementGoal::from_vector_field(image).unwrap()
+}
+
+#[test]
+fn custom_entry_point() -> Result<(), Box<dyn Error>> {
+    let dimensions = ImageDimensions::try_new(3, 4)?;
+
+    let dispatcher = compute::create_dispatcher_block(&Config {
+        image_dimensions: dimensions,
+    })?;
+    let mut algorithm = dispatcher.create_displacement_goal(
+        Default::default(),
+        &CreateDisplacementGoalParameters {
+            pipeline_operation: CreateDisplacementGoalPipelineOperation::Set(PipelineConfig {
+                shader_config: CreateDisplacementGoalShaderConfig {
+                    content: test_util::shader::load_create_displacement_goal_shader_content(
+                        "custom_entry_point",
+                    ),
+                    entry_point: Cow::Borrowed(CUSTOM_SHADER_ENTRY_POINT),
+                },
+                workgroup_grid: WorkgroupGridConfig::BlockSize {
+                    width: NonZeroU32::new(2).unwrap(),
+                    height: NonZeroU32::new(3).unwrap(),
+                },
+            }),
+        },
+    );
+    assert_step_until_success(algorithm.as_mut(), OutputStatus::FinalFullOutput)?;
+
+    let output = algorithm.full_output_block().unwrap();
+    assert!(output.input_displacement_goal.is_none());
+    assert!(output.permutation.is_none());
+    assert!(output.image.is_none());
+    assert_eq!(
+        output.output_displacement_goal,
+        make_filled_rectangle_displacement_goal(&dimensions, &dimensions)
+    );
+    assert!(algorithm.full_output_block().is_none());
+    Ok(())
+}
--- test_data/shader/create_displacement_goal/custom_entry_point.wgsl
+++ test_data/shader/create_displacement_goal/custom_entry_point.wgsl
@@ -1,4 +1,4 @@
-@compute @workgroup_size(16, 16, 1)
+@compute @workgroup_size(2, 3, 1)
 fn entry_point(@builtin(global_invocation_id) global_id: vec3<u32>) {
   let coords : vec2<i32> = vec2<i32>(global_id.xy);
   textureStore(output_displacement_goal, coords, vec4<u32>(1u, 2u, 3u, 4u));
