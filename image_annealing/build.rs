d77d1f8ec7c9a1e7ff586f522a0a51461738c980
Bernard Llanos <llanbe_t3mp@yahoo.ca>
2023-07-30 09:34:26 -0600

    Update build script based on changes to the shader CLI

--- image_annealing/build.rs
+++ image_annealing/build.rs
@@ -5,7 +5,7 @@ fn main() {
     let config = image_annealing_shader_cli::output::write_default_files(Some(directory)).unwrap();
     println!(
         "cargo:rustc-env=COUNT_SWAP_SHADER={}",
-        config.count_swap.as_ref().unwrap().display()
+        config.count_swap.as_ref().unwrap().0.display()
     );
     println!(
         "cargo:rustc-env=CREATE_DISPLACEMENT_GOAL_DEFAULT_SHADER={}",
@@ -13,19 +13,20 @@ fn main() {
             .create_displacement_goal_default
             .as_ref()
             .unwrap()
+            .0
             .display()
     );
     println!(
         "cargo:rustc-env=CREATE_PERMUTATION_SHADER={}",
-        config.create_permutation.as_ref().unwrap().display()
+        config.create_permutation.as_ref().unwrap().0.display()
     );
     println!(
         "cargo:rustc-env=PERMUTE_SHADER={}",
-        config.permute.as_ref().unwrap().display()
+        config.permute.as_ref().unwrap().0.display()
     );
     println!(
         "cargo:rustc-env=SWAP_SHADER={}",
-        config.swap.as_ref().unwrap().display()
+        config.swap.as_ref().unwrap().0.display()
     );
 
     println!("cargo:rerun-if-changed=build.rs");
